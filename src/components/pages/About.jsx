d928d8171fa07d128b68d34950e693eaf902ea04
Elias Rivera <eliasjohnrivera@gmail.com>
2023-11-01 13:43:29 -0700

    deleted redudant code

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ import avatar from '../../assets/images/github-avatar.png';
 export default function About() {
     return (
         <div>
-            <h1>About Me</h1>
+            <h1>About</h1>
             <p>Recent Full Stack Software Engineer graduate from UC San Diego's Extended Studies Coding Bootcamp. Proficient in a wide range of web technologies and frameworks, with an ability to quickly adapt to new technology stacks.</p>
             <p>With a diverse background in hospitality, public health, and research, I'm dedicated to creating a significant contribution. These roles have ensured me with the skills to contribute innovate solutions to dynamic problems. My tenure has strengthened my analytical abilities, research acumen, and capacity to carry out complex activities smoothly and adaptively.</p>
             <p>I'm now eager to leverage my coding skills and multifaceted background to contribute meaningfully to innovative projects. Let's connect and explore how I can be a valuable asset to your team!</p>
