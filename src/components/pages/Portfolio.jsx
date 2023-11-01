7453f12cb6dec215092dd40ed64a52c55e69c77e
Elias Rivera <eliasjohnrivera@gmail.com>
2023-10-31 19:01:51 -0700

    updated portfolio section with react boostrap and css styling

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,9 +1,12 @@
 import Accordion from 'react-bootstrap/Accordion';
+import { VscGithubAlt } from "react-icons/vsc";
+import { TbWorldWww } from "react-icons/tb"
 
 import novelista from '../../assets/images/novelista.png';
 import edurater from '../../assets/images/edurater.png';
 import mvmnt from '../../assets/images/mvmnt.png';
 import filmify from '../../assets/images/filmify.png';
+
 
 export default function Portfolio() {
     return (
@@ -13,32 +16,64 @@ export default function Portfolio() {
             <Accordion.Item eventKey="0">
                 <Accordion.Header>NOVELISTA</Accordion.Header>
                 <Accordion.Body>
-                <a className='a-novelista' href="https://github.com/mslzbry/mvmnt">novelista GitHub Repository</a>
-                <a className='a-novelista' href="https://fast-scrubland-44894-b727f94b7e1a.herokuapp.com/">novelista Deployed Site</a>
+                <a href="https://github.com/eliasjrivera/Novelista" target="_blank">
+                    <li className="logo">
+                        <VscGithubAlt />
+                    </li>
+                </a> &nbsp;
+                <a href="https://github.com/eliasjrivera/Novelista" target="_blank">
+                    <li className="logo">
+                        <TbWorldWww />
+                    </li>
+                </a>
                 <img src={novelista} alt='novelista'className="novelista"/>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>EduRATEr</Accordion.Header>
                 <Accordion.Body>
-                <a className='a-edurater' href="https://github.com/mslzbry/mvmnt">mvmnt GitHub Repository</a>
-                <a className='a-edurater' href="https://fast-scrubland-44894-b727f94b7e1a.herokuapp.com/">mvmnt Deployed Site</a>
-                <img src={edurater} alt='mvmnt'className="mvmnt"/>
+                <a href="https://github.com/Pinto006/rate-my-educator" target="_blank">
+                    <li className="logo">
+                        <VscGithubAlt />
+                    </li>
+                </a> &nbsp;
+                <a href="https://damp-wildwood-31060-690d212e58d9.herokuapp.com/" target="_blank">
+                    <li className="logo">
+                        <TbWorldWww />
+                    </li>
+                </a>
+                <img src={edurater} alt='edurater'className="edurater"/>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
                 <Accordion.Header>mvmnt</Accordion.Header>
                 <Accordion.Body>
-                <a className='a-mvmnt' href="https://github.com/mslzbry/mvmnt">mvmnt GitHub Repository</a>
-                <a className='a-mvmnt' href="https://fast-scrubland-44894-b727f94b7e1a.herokuapp.com/">mvmnt Deployed Site</a>
+                <a href="https://github.com/mslzbry/mvmnt" target="_blank">
+                    <li className="logo">
+                        <VscGithubAlt />
+                    </li>
+                </a> &nbsp;
+                <a href="https://github.com/mslzbry/mvmnt" target="_blank">
+                    <li className="logo">
+                        <TbWorldWww />
+                    </li>
+                </a>
                 <img src={mvmnt} alt='mvmnt'className="mvmnt"/>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="3">
                 <Accordion.Header>Filmify</Accordion.Header>
                 <Accordion.Body>
-                <a className='a-filmify' href="https://github.com/eliasjrivera/Filmify">Filmify GitHub Repository</a>
-                <a className='a-filmify' href="https://eliasjrivera.github.io/Filmify/">Filmify Deployed Site</a>
+                <a href="https://github.com/eliasjrivera/Filmify" target="_blank">
+                    <li className="logo">
+                        <VscGithubAlt />
+                    </li>
+                </a> &nbsp;
+                <a href="https://eliasjrivera.github.io/Filmify/" target="_blank">
+                    <li className="logo">
+                        <TbWorldWww />
+                    </li>
+                </a>
                 <img src={filmify} alt='filmify'className="filmify"/>   
                 </Accordion.Body>
             </Accordion.Item>
