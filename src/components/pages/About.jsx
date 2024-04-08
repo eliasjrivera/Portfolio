2b89b579ec68b8d90374bdfe61bc637d7fb46a9f
Elias Rivera <eliasjohnrivera@gmail.com>
2024-04-07 21:57:01 -0700

    updated avatar

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 // switch name to About after website functions
 // Home = About
-import avatar from '../../assets/images/avatar.jpg';
+import avatar from '../../assets/images/avatar.JPG';
 
 export default function About() {
     return (
